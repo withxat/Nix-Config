db237bcc9669e6a0c983268e3771efbe8feefe0f
Xat <i@xat.sh>
2026-06-09 19:04:20 +0800

    feat: accomplish server config

--- hosts/oyama/configuration.nix
+++ hosts/oyama/configuration.nix
@@ -3,5 +3,42 @@
 {
   system.stateVersion = "26.05";
 
-  services.openssh.enable = true;
+  networking.hostName = "oyama";
+
+  boot.loader.grub = {
+    enable = true;
+    device = "/dev/vda";
+  };
+
+  zramSwap.enable = true;
+
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = false;
+      PermitRootLogin = "prohibit-password";
+    };
+  };
+
+  users.users.xat = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMyy5imzQ6j9ZQ5HfMIljQxVGqzP8C9CNAVAGsp0CFLf"
+    ];
+  };
+
+  security.sudo.wheelNeedsPassword = false;
+
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
+  environment.systemPackages = with pkgs; [
+    git
+    curl
+    vim
+    htop
+  ];
 }
--- hosts/oyama/disko.nix
+++ hosts/oyama/disko.nix
@@ -3,21 +3,14 @@
     disk.vda = {
       type = "disk";
       device = "/dev/vda";
+
       content = {
         type = "gpt";
+
         partitions = {
           boot = {
             size = "1M";
             type = "EF02";
-          };
-
-          ESP = {
-            size = "1G";
-            content = {
-              type = "filesystem";
-              format = "ext4";
-              mountpoint = "/boot";
-            };
           };
 
           root = {
