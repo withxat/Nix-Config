15f000aed0b84c13b4f374546e58a79a3e685283
Xat <i@xat.sh>
2026-06-09 18:51:08 +0800

    fix: wrong host name

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
   };
 
   outputs = { self, nixpkgs, ... }: {
-    nixosConfigurations.misaka = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.oyama = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
 
       modules = [
