5d896c18a5452439f395797d0b001891cca55d45
Xat <i@xat.sh>
2026-09-14 19:56:59 +0800

    chore(flag): deploy continuous local semantic analysis

--- hosts/ricardo/flag.nix
+++ hosts/ricardo/flag.nix
@@ -3,6 +3,7 @@
     enable = true;
     tailscaleServe = true;
     visualWorker.enable = true;
+    semanticWorker.enable = true;
     allowedOrigins = [ "https://ricardo.tail3921e8.ts.net:8443" ];
   };
 }
